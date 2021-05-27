45510da1b3545a0e1cdb1acb15e53eeece8cbf3c
dante <dantinr@gmail.com>
2021-05-27 17:04:01 +0800

    随机字符串

--- 5-27/str_rand.php
+++ 5-27/str_rand.php
@@ -1,19 +1,15 @@
 <?php
     //生成随机字符串  $length是 返回的字符串长度
     function str_rand($length=8){
-
         $str0 = 'ABCDEFGHIJKMNPQRSTYVWXYZabcdefghijkmnpqrstuvwxyz23456789';
         $str = "";
         for($i=0;$i<$length;$i++){
             $num = mt_rand(0,55);
-            echo "随机数: ". $num;echo '</br>';
-            echo "随机的字母： ".$str0[$num];echo '</br>';
             $c = $str0[$num];
             $str .= $c;
         }
         return $str;
     }
-
     
     var_dump(str_rand());
 
