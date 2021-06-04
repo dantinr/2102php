921405669ca91995c004ae74b98e8b9523acbc72
dante <dantinr@gmail.com>
2021-06-04 10:32:41 +0800

    首页 尾页

--- 6-4/list.php
+++ 6-4/list.php
@@ -22,31 +22,32 @@
     //计算下一页
     $next = $page + 1;
 
-    echo "当前页： ". $page; echo '</br>';
+    //计算总页数  ceil(总记录数 / $size)
+    //获取总记录数
+    $sql = "select count(1) as total from p_goods";
+    $res = mysqli_query($link,$sql);
+    $row = mysqli_fetch_row($res);
+    $total_number = $row[0];        //总记录数
+    $total_page = ceil($total_number / $size);  //总页数
+
 
     $start = ($page - 1) *  $size;              //跳过的行数  从多少行开始
-    echo "start: " . $start;echo '</br>';
-
     $sql = "select goods_id,goods_name from p_goods limit {$start},{$size}";
-    echo "sql: ". $sql;echo '</br>';
-
     $res = mysqli_query($link,$sql);
-
     $rows = mysqli_fetch_all($res,MYSQLI_ASSOC);
 
     echo "<ul>";
-
     foreach($rows as $k=>$v){
         echo "<li>";
         echo  $v['goods_id'] . ' ' .$v['goods_name'];
         echo "</li>";
     }
-
     echo "</ul>";
-
-
 ?>
 
 <hr>
-<a href="list.php?page=<?php echo $prev;?>">上一页</a> | <a href="list.php?page=<?php echo $next;?>">下一页</a>
+<a href="list.php?page=1">首页</a> |
+<a href="list.php?page=<?php echo $prev;?>">上一页</a> |
+<a href="list.php?page=<?php echo $next;?>">下一页</a> |
+<a href="list.php?page=<?php echo $total_page;?>">尾页</a> |
 
